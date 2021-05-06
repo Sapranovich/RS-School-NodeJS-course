0a167d3e2057a3049e71fc5749ebb5a3d82819e9
Sapranovich Andrey <SapranovichAndrey@yandex.ru>
2021-05-06 11:31:03 +0300

    feat: add caesarShift function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const argv = require("minimist")(process.argv);
 const { pipeline } = require("stream");
-const Stream = require('stream');
+const Stream = require("stream");
 
 const fs = require("fs");
 const { promisify } = require("util");
@@ -16,17 +16,37 @@ const params = {
   action: argv.a || argv.action,
 };
 
+// ============== финкция  Caesar shift ==============
+function caesarShift(text, shift) {
+  if (shift < 0) {
+    return caesarShift(text, shift + 26);
+  }
+  let resault = "";
+  for (let i = 0; i < text.length; i++) {
+    let letter = text[i];
+    if (letter.match(/[a-z]/i)) {
+      const code = text.charCodeAt(i);
+      if (code >= 65 && code <= 90) {
+        letter = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+      } else if (code >= 97 && code <= 122) {
+        letter = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+      }
+    }
+    resault += letter;
+  }
+  return resault;
+}
+// ===================================================
+
 const readStream = fs.createReadStream(params.input);
 const writeStream = fs.createWriteStream(params.output);
 
 const transformStream = new Stream.Transform({
-	transform(chunk, encoding, callback) {
-		const transformed = chunk.toString();
-		callback(null, transformed);
-	},
+  transform(chunk, encoding, callback) {
+    const transformText = caesarShift(chunk.toString(), params.shift);
+    callback(null, transformText);
+  },
 });
-
-
 
 (async () => {
   try {
